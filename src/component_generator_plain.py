5be69006191d86c64c5bfead1f20371453d288c7
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2022-06-27 14:48:44 -0700

    updates

---
+++ src/component_generator_plain.py
@@ -0,0 +1,23 @@
+"""
+Model Generator for OpenSees ~ model
+"""
+
+#                          __
+#   ____  ____ ___  ____ _/ /
+#  / __ \/ __ `__ \/ __ `/ /
+# / /_/ / / / / / / /_/ /_/
+# \____/_/ /_/ /_/\__, (_)
+#                /____/
+#
+# https://github.com/ioannis-vm/OpenSees_Model_Generator
+
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+
+@dataclass
+class ComponentGeneratorPlain:
+    """
+    todo - work in progress
+    """
+    pass
