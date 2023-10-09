7f5b56f12fe18ffa46bbb9b37b9f74b2135d03f3
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2023-10-08 20:39:38 -0700

    improve deploy workflow
    No need to install `osmg` again to get its version, we can directly
    query PyPI.

---
+++ src/osmg/get_latest_pypi_version.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def get_latest_version(package_name):
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    
+    try:
+        response = requests.get(url)
+        package_info = response.json()
+        latest_version = package_info['info']['version']
+        return latest_version
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+if __name__ == '__main__':
+    package = 'osmg'
+    version = get_latest_version(package)
+    print(version)
