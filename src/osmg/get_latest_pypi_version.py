5935eb37201945c1c32198fb738e9f388097c21e
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2023-10-08 20:42:52 -0700

    linting `get_latest_pypi_version.py`

--- src/osmg/get_latest_pypi_version.py
+++ src/osmg/get_latest_pypi_version.py
@@ -3,18 +3,18 @@ import requests
 
 def get_latest_version(package_name):
     url = f"https://pypi.org/pypi/{package_name}/json"
-    
+
     try:
         response = requests.get(url)
         package_info = response.json()
-        latest_version = package_info['info']['version']
+        latest_version = package_info["info"]["version"]
         return latest_version
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
 
 
-if __name__ == '__main__':
-    package = 'osmg'
+if __name__ == "__main__":
+    package = "osmg"
     version = get_latest_version(package)
     print(version)
