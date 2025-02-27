4b76a3f7b6705fbf2c2f965c82ad9924fc0ec1eb
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2025-02-26 17:07:53 -0800

    Add and multiply methods.

--- src/osmg/analysis/common.py
+++ src/osmg/analysis/common.py
@@ -17,6 +17,34 @@ class ConcentratedValue(tuple[float, ...]):
         """
         return f"{self.__class__.__name__}({', '.join(map(str, self))})"
 
+    def __add__(self, other: ConcentratedValue) -> ConcentratedValue:
+        """
+        Add two ConcentratedValue instances element-wise.
+
+        Args:
+          other: Another ConcentratedValue instance.
+
+        Returns:
+          A new ConcentratedValue instance with the sum of corresponding elements.
+        """
+        if not isinstance(other, ConcentratedValue):
+            return NotImplemented
+        return ConcentratedValue(a + b for a, b in zip(self, other))
+
+    def __mul__(self, scalar: float) -> ConcentratedValue:
+        """
+        Multiply each element of ConcentratedValue by a scalar.
+
+        Args:
+          scalar: A numeric value to multiply each element by.
+
+        Returns:
+          A new ConcentratedValue instance with each element scaled.
+        """
+        if not isinstance(scalar, (int, float)):
+            return NotImplemented
+        return ConcentratedValue(a * scalar for a in self)
+
 
 class PointLoad(ConcentratedValue):
     """Point load."""
@@ -44,3 +72,31 @@ class UDL(tuple[float, ...]):
           A simple string representation of the object.
         """
         return f"{self.__class__.__name__}({', '.join(map(str, self))})"
+
+    def __add__(self, other: UDL) -> UDL:
+        """
+        Add two UDL instances element-wise.
+
+        Args:
+          other: Another UDL instance.
+
+        Returns:
+          A new UDL instance with the sum of corresponding elements.
+        """
+        if not isinstance(other, UDL):
+            return NotImplemented
+        return UDL(a + b for a, b in zip(self, other))
+
+    def __mul__(self, scalar: float) -> UDL:
+        """
+        Multiply each element of UDL by a scalar.
+
+        Args:
+          scalar: A numeric value to multiply each element by.
+
+        Returns:
+          A new UDL instance with each element scaled.
+        """
+        if not isinstance(scalar, (int, float)):
+            return NotImplemented
+        return UDL(a * scalar for a in self)
